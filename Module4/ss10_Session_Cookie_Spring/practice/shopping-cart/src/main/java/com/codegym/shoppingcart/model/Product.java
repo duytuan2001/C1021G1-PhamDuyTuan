ad4c6091c7860c827d2dec06bb4b633fa0c2bc22
duytuan2001 <p.duy.t.123@gmail.com>
2022-03-16 08:23:37 +0700

    push

---
+++ Module4/ss10_Session_Cookie_Spring/practice/shopping-cart/src/main/java/com/codegym/shoppingcart/model/Product.java
@@ -0,0 +1,68 @@
+package com.codegym.shoppingcart.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ Module4/ss11_Web_Service_RESTful/practice/spring-customer-management-restful/src/main/java/com/codegym/customermanagement/repository/ICustomerRepository.java
@@ -0,0 +1,8 @@
+package com.codegym.customermanagement.repository;
+
+import com.codegym.customermanagement.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
